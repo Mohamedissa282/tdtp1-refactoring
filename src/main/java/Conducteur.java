1ec88e75a64a9a304580658f940f110c212e99de
Mohamed Essa <mohamedissa4730@gmail.com>
2026-01-30 11:33:55 +0100

    J'ai ajouté des commantaires dans classe Conducteur

--- src/main/java/Conducteur.java
+++ src/main/java/Conducteur.java
@@ -1,16 +1,38 @@
+
 public class Conducteur {
+    /**
+     * le nom du conducteur
+     */
     private String name;
+    /**
+     * son age
+     */
     private int years;
 
+    /**
+     * constructeur
+     * @param name
+     * @param years
+     */
     public Conducteur(String name, int years) {
         this.name = name;
         this.years = years;
     }
 
+    /**
+     * verification de l'age
+     * @return
+     */
     public boolean estAdulte() {
         return years >= 18;
     }
 
+    /**
+
+     *Tente de démarrer la voiture si le conducteur est adulte
+     *  Affiche un message de refus si le conducteur est mineur
+     * @param voiture
+     */
     public void demarrerVoiture(Voiture voiture) {
         if (estAdulte()) {
             System.out.println(name + " démarre la voiture.");
@@ -19,10 +41,20 @@ public class Conducteur {
         }
     }
 
+    /**
+     * Arrête la voiture spécifiée
+     * @param voiture
+     */
     public void arreterVoiture(Voiture voiture) {
         System.out.println(name + " arrête la voiture.");
     }
 
+    /**
+     * Modifie la vitesse de la voiture pour atteindre une vitesse cible
+     * Cette méthode gère l'accélération ou le freinage progressif
+     * @param voiture
+     * @param nouvelleVitesse
+     */
     public void changerVitesse(Voiture voiture, int nouvelleVitesse) {
         System.out.println(name + " change la vitesse de la voiture à " + nouvelleVitesse);
         if (voiture.getVitesse() >= nouvelleVitesse) {
