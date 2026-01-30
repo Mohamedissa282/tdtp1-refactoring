3f45abbc63047a741355b788f27dd46e89dc47c7
Mohamed Essa <mohamedissa4730@gmail.com>
2026-01-30 11:23:55 +0100

    J'ai ajouté des commantaires dans classe Voiture

--- src/main/java/Voiture.java
+++ src/main/java/Voiture.java
@@ -1,19 +1,48 @@
+
 public class Voiture {
+    /**
+     * model de la voiture
+     */
     private String modele;
+    /**
+     * color de la voiture
+     */
     private static String color;
+    /**
+     * vitesse
+     */
     private int vitesse;
+    /**
+     *
+     */
     private static final int PAS_VITESSE = 10;
+    /**
+     * intialisation la vitesse maximum avec 120
+     */
     private static final int VITESSE_MAX = 120;
+
+    /**
+     * constructeur
+     * @param model
+     * @param color
+     */
     public Voiture(String model, String color) {
         this.modele = model;
         this.color = color;
         this.vitesse = 0;
     }
 
+    /**
+     * on recupere la vitesse
+     * @return
+     */
     public int getVitesse() {
         return this.vitesse;
     }
 
+    /**
+     * fonctionne accelerer
+     */
     public void accelerer() {
         if (vitesse + PAS_VITESSE <= VITESSE_MAX) {
             vitesse += PAS_VITESSE;
@@ -30,6 +59,9 @@ public class Voiture {
         System.out.println("Vitesse actuelle : " + vitesse);
     }
 
+    /**
+     * fonction relantir
+     */
     public void ralentir() {
         if (vitesse - PAS_VITESSE >= 0) {
             vitesse -= PAS_VITESSE;
